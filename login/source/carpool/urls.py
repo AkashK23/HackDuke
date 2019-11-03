dc6dc4eb47e8d89fed8fa13655ea18bd08ce712c
AkashK23 <34080339+AkashK23@users.noreply.github.com>
2019-11-03 07:24:54 -0500

    User URL works with "/"
    added func

--- login/source/carpool/urls.py
+++ login/source/carpool/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('<user>', views.IndexView.as_view(), name='index'),
+    path('<user>/', views.IndexView.as_view(), name='index'),
     path('<user>/driverform', views.DriverForm.as_view(), name='driverform'),
     path('<user>/passengerform', views.PassengerForm.as_view(), name='passengerform')
 ]
