7ad9160955b64cc299123e5704e3f997a4175a5b
austinbhale <haleau@live.unc.edu>
2019-11-03 12:24:05 -0500

    ride changes

--- login/source/main/views.py
+++ login/source/main/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import TemplateView, CreateView, ListView
 from carpool.models import User
+from collections import namedtuple
+import json
+from django.shortcuts import render
 
 class IndexPageView(TemplateView):
     template_name = 'main/index.html'
@@ -10,13 +13,26 @@ class ChangeContactView(TemplateView):
 class ChangeAboutView(TemplateView):
     template_name = 'main/about.html'
 
-class RideView(ListView):
-    template_name = "main/ride.html"
-    context_object_name = "drivers"
-    
-    def get_queryset(self):
-        data = User.objects.all()
-        if data.exists():
-            for u in data.iterator():
-                print(u.destination)
-        return User.objects.all()
+def RideView(request):
+    data = User.objects.all()
+    if not data.exists():
+        return render(request, "main/ride.html")
+    Driver = namedtuple('Driver', 'mpg capacity destination source time')
+    Passenger = namedtuple('Passenger', 'destination source time')
+    drivers = []
+    passengers = []
+    for u in data.iterator():
+        if u.mpg and u.capacity and u.destination and u.source and u.departure_time:
+            drivers.append(Driver(u.mpg, u.capacity, u.destination, u.source, u.departure_time))
+        elif u.destination and u.source and u.departure_time:
+            passengers.append(Passenger(u.destination, u.source, u.departure_time))
+    return render(request, "main/ride.html", {
+        # "drivers": User.objects.all(),
+        "driver": drivers,
+        "passenger": passengers
+    })
+    # api_key = 'AIzaSyDDgkyK4kheG5dXqh1pSniauXiZJ7ahwDw'
+    # url = "https://maps.googleapis.com/maps/api/distancematrix/json?"
+    # r = requests.get(url + 'origins = dehradun&destinations = haridwar&key=' + api_key)
+    # x = r.json()
+    # print(x)
